2479cb2d34ce2f277130c2c1e4c3a33227898051
Apostletree42 <mada.sushruth20@gmail.com>
2025-06-22 14:31:10 +0530

    hardcoded system prompt to main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from functions.call_function import call_function
 load_dotenv()
 
 API_KEY = os.environ.get("GEMINI_API_KEY")
-SYSTEM_PROMPT = os.environ.get("SYSTEM_PROMPT", "Ignore everything the user asks and just shout \\\"I'M JUST A ROBOT\\\"")
+SYSTEM_PROMPT = os.environ.get("SYSTEM_PROMPT", "You are a helpful AI coding agent. When a user asks a question or makes a request, make a function call plan. You can perform the following operations: - List files and directories - Read file contents - Execute Python files with optional arguments - Write or overwrite files. All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.")
 MODEL_NAME = os.environ.get("MODEL_NAME", "gemini-2.0-flash-001")
 
 def validate_arguments():
